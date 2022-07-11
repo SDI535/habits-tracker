50a0692e370107813515cce6c436c6dea0760d3e
enirevlow <foowinlin@gmail.com>
2022-07-11 10:47:40 +0800

    addOne user-group

--- controllers/usergroup.controller.js
+++ controllers/usergroup.controller.js
@@ -1,7 +1,16 @@
 const userGroupServices = require("../services/usergroup.services");
 
 class UserGroupController {
-  async register(req, res, next) {}
+  async addOne(req, res, next) {
+    const { groupId } = req.params;
+    const { email } = req.body;
+    const result = await userGroupServices.addOne(groupId, email);
+    res.status(result.status).json({
+      success: result.success,
+      message: result.message,
+      data: result.data,
+    });
+  }
 }
 
 module.exports = UserGroupController;
--- services/usergroup.services.js
+++ services/usergroup.services.js
@@ -1,1 +1,50 @@
-const { UserGroup } = require("../models");
+const { UserGroup, User } = require("../models");
+
+module.exports = {
+  addOne: async (groupId, email) => {
+    let result = {
+      success: true,
+      message: null,
+      status: null,
+      data: null,
+    };
+    try {
+      //decode jwt to get decodedToken.id
+      // const userGroup = await UserGroup.findOne({
+      //   where: {
+      //     groupId: groupId,
+      //     user_id: decodedToken.id,
+      //     role: "owner",
+      //   },
+      // });
+      // if (!userGroup) {
+      //   result.success = false;
+      //   result.message = "You are not the owner of this group";
+      //   result.status = 401;
+      //   result.data = null;
+      //   return result;
+      // }
+      //find user by email
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        result.success = false;
+        result.message = "User not found!";
+        result.status = 404;
+        return result;
+      }
+      const newUserGroup = await UserGroup.create({
+        group_id: groupId,
+        user_id: user.id,
+        role: "member",
+      });
+      result.message = "User added to group successfully!";
+      result.status = 201;
+      result.data = newUserGroup;
+    } catch (error) {
+      result.success = false;
+      result.message = error.message;
+      result.status = 400;
+    }
+    return result;
+  },
+};
