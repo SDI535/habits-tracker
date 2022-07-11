a104158353351128d06181e71ad1ffdd0afe4ccf
enirevlow <foowinlin@gmail.com>
2022-07-11 10:43:53 +0800

    fix route for add habit

--- routes/habitgroup.routes.js
+++ routes/habitgroup.routes.js
@@ -2,22 +2,20 @@ const express = require("express");
 const router = express.Router();
 const HabitGroupController = require("../controllers/habitgroup.controller");
 
-const habitgroupcontroller = new HabitGroupController();
+// const habitgroupcontroller = new HabitGroupController();
 
 const verifyToken = require("../middleware/userauth.middleware");
 
 //GET all habits in a group
-//need to verify if it's user is in the group, in order to view
 router.get("/groups/:groupId/habits", habitgroupcontroller.listHabits);
 
 //ADD one habit to a group
-//need middleware to verify if owner or not
-//need to verify if it's user is in the group, in order to add habit to group
+// need to verify if user is in the group and owner of habit, in order to add habit to group
 
-router.put("/groups/:groupId/habit", habitgroupcontroller.addOne);
+router.post("/groups/:groupId/habit", habitgroupcontroller.addOne);
 
 //DELETE one habit from a group
-//need middleware to verify if owner of habit and in the group, to remove from group
+//need to verify if owner of habit and in the group, to remove from group
 router.delete(
   "/groups/:groupId/habits/:habitId",
   usergroupcontroller.deleteOne
