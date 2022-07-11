2bbdb977d1f07790cd8f8d994573517d73651d49
enirevlow <foowinlin@gmail.com>
2022-07-11 10:41:03 +0800

    fix route for add mem

--- routes/usergroup.routes.js
+++ routes/usergroup.routes.js
@@ -7,25 +7,18 @@ const usergroupcontroller = new UserGroupController();
 const verifyToken = require("../middleware/userauth.middleware");
 
 //GET all members in a group
-router.get(
-  "/groups/:groupId/members",
-  verifyToken,
-  usergroupcontroller.listMembers
-);
+router.get("/groups/:groupId/members", usergroupcontroller.listMembers);
 
 //ADD one member to a group
-//need middleware to verify if group owner or not
-router.put("/groups/:groupId/members", usergroupcontroller.addOne);
+router.post("/groups/:groupId/members", usergroupcontroller.addOne);
 
 //DELETE one member from a group
-//need middleware to verify if group owner or not, to remove own member or others
 router.delete(
   "/groups/:groupId/members/:memberId",
   usergroupcontroller.deleteOne
 );
 
 //UPDATE role of member in a group
-//need middleware to verify if group owner or not
 router.put("/groups/:groupId/members/:memberId", usergroupcontroller.updateOne);
 
 module.exports = router;
