227d69f6f0dc9503ec771758de5ca25bf6812aa3
enirevlow <foowinlin@gmail.com>
2022-07-13 12:27:51 +0800

    get groups and habits

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/user.controller");
-const UserGroupController = require("../controllers/usergroup.controller");
-const HabitController = require("../controllers/habit.controller");
-const authenticateJWT = require("../middleware/userauth.middleware")
+const authenticateJWT = require("../middleware/userauth.middleware");
 
 const usercontroller = new UserController();
-const usergroupcontroller = new UserGroupController();
-const habitcontroller = new HabitController();
 
 router.get("/", (req, res) => {
   return res.send("You have reached the landing page of habitsTracker");
@@ -20,12 +16,12 @@ router.post("/register", usercontroller.register);
 router.post("/login", usercontroller.login);
 
 //Testing purposes to retrieve 1 record of user details with auth
-router.get("/userDetails",authenticateJWT, usercontroller.userDetails);
+router.get("/userDetails", authenticateJWT, usercontroller.userDetails);
 
 // //GET all groups a user belongs to
-// router.get("/:userId/groups", usergroupcontroller.listGroups);
+router.get("/:userId/groups", usercontroller.listGroups);
 
 // //GET all habits for a user
-// router.get("/:userId/habits", habitcontroller.list);
+router.get("/:userId/habits", usercontroller.listHabits);
 
 module.exports = router;
