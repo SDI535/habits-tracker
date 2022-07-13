bc4df18aee71ee9b1b4153ba01bf939315cbd3be
joy_foo <42211730+enirevlow@users.noreply.github.com>
2022-07-13 12:44:56 +0800

    modify comments
    remove double // from comments

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -18,10 +18,10 @@ router.post("/login", usercontroller.login);
 //Testing purposes to retrieve 1 record of user details with auth
 router.get("/userDetails", authenticateJWT, usercontroller.userDetails);
 
-// //GET all groups a user belongs to
+//GET all groups a user belongs to
 router.get("/:userId/groups", authenticateJWT, usercontroller.listGroups);
 
-// //GET all habits for a user
+//GET all habits for a user
 router.get("/:userId/habits", authenticateJWT, usercontroller.listHabits);
 
 module.exports = router;
