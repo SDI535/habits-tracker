6ec6f1caa34d842da22dcc93594e7d442b2b8b54
lee-jun-hui <lee@junhui.sg>
2022-07-14 11:10:09 +0800

    junhui JWT
    junhui JWT

--- routes/group.routes.js
+++ routes/group.routes.js
@@ -1,22 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const GroupController = require("../controllers/group.controller");
+const authenticateJWT = require("../middleware/userauth.middleware");
 
 const groupcontroller = new GroupController();
 
-//middleware to verify before information can be accessed
-// const verifyToken = require("../middleware/userauth.middleware");
-
 //CREATE one group
 // router.post("/:userId/groups", verifyToken, groupcontroller.createOne);
-router.post("/:userId/groups", groupcontroller.createOne);
+router.post("/:userId/groups", authenticateJWT,groupcontroller.createOne);
 
 //DELETE one group
 // router.delete("/:userId/groups/:groupId", verifyToken, groupcontroller.deleteOne);
-router.delete("/:userId/groups/:groupId", groupcontroller.deleteOne);
+router.delete("/:userId/groups/:groupId", authenticateJWT,groupcontroller.deleteOne);
 
 //UPDATE one group
 // router.put("/:userId/groups/:groupId", verifyToken, groupcontroller.updateOne);
-router.put("/:userId/groups/:groupId", groupcontroller.updateOne);
+router.put("/:userId/groups/:groupId", authenticateJWT,groupcontroller.updateOne);
 
 module.exports = router;
--- routes/usergroup.routes.js
+++ routes/usergroup.routes.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const UserGroupController = require("../controllers/usergroup.controller");
+const authenticateJWT = require("../middleware/userauth.middleware");
 
 const usergroupcontroller = new UserGroupController();
 
-// const verifyToken = require("../middleware/userauth.middleware");
-
 //GET all members in a group
-// router.get("/groups/:groupId/members", usergroupcontroller.listMembers);
+router.get("/groups/:groupId/members", authenticateJWT, usergroupcontroller.listMembers);
 
 //ADD one member to a group
-router.post("/groups/:groupId/members/:userId", usergroupcontroller.addOne);
+router.post("/groups/:groupId/members/:userId", authenticateJWT, usergroupcontroller.addOne);
 
 //DELETE one member from a group
-router.delete(
-  "/groups/:groupId/members/:userId",
-  usergroupcontroller.deleteOne
-);
+router.delete("/groups/:groupId/members/:userId", authenticateJWT, usergroupcontroller.deleteOne);
 
 //UPDATE role of member in a group
-router.put("/groups/:groupId/members/:userId", usergroupcontroller.updateOne);
+router.put("/groups/:groupId/members/:userId", authenticateJWT, usergroupcontroller.updateOne);
 
 module.exports = router;
