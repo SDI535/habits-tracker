f1b9b1deeab0912f7541201bdda1260c313da955
Christina Choo <crstntina@Christinas-MacBook-Air.local>
2022-07-14 10:25:50 +0800

    update JWT authentication

--- routes/habit.routes.js
+++ routes/habit.routes.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const HabitController = require("../controllers/habit.controller");
+const authenticateJWT = require("../middleware/userauth.middleware");
 
 const habitcontroller = new HabitController();
 
-const verifyToken = require("../middleware/userauth.middleware");
-
 //CREATE one habit
-router.post("/:userId/habits", habitcontroller.createOne);
+router.post("/:userId/habits", authenticateJWT, habitcontroller.createOne);
 
 // DELETE one habit
-router.delete("/:userId/habits/:habitId",habitcontroller.deleteOne);
+router.delete("/:userId/habits/:habitId", authenticateJWT, habitcontroller.deleteOne);
 
 //UPDATE one habit
-router.put("/:userId/habits/:habitId", habitcontroller.updateOne);
+router.put("/:userId/habits/:habitId", authenticateJWT, habitcontroller.updateOne);
 
 module.exports = router;
